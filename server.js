ff4c09ff09c4cbd37d45136a5c5b33b3d4f4da46
Christianngono <christianngonoabanda384@gmail.com>
2024-05-07 08:08:09 +0200

    To change it more

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const app = express();
 const path = require('path');
 const fs = require('fs');
 const bodyParser = require('body-parser');
+
+// Chemin vers les données des livres
 const booksDataPath = path.join(__dirname, 'booksData.json');
 const booksData = require(booksDataPath);
 
@@ -25,19 +27,34 @@ app.get('/addBook', function(req, res) {
 });
 
 // Route pour ajouter un livre
-app.post('/addBook', function(res) {
+app.post('/addBook', function(req, res) {
     const highestId = Math.max(...booksData.map(book => book.id));
     const newBookId = highestId + 1;
+    const newBookTitle = req.body.title;
+    const newBookAuthor = req.body.author;
+    const newBookGenre = req.body.genre;
+    const newBookDueDate = req.body.dueDate;
     booksData.push({
         id: newBookId,
-        title: 'Titre du livre',
-        author: 'Auteur du livre',
-        genre: 'Genre du livre',
+        title: newBookTitle,
+        author: newBookAuthor,
+        genre: newBookGenre,
         borrowed: false,
-        dueDate: ''
+        dueDate: newBookDueDate,
     });
     fs.writeFileSync(booksDataPath, JSON.stringify(booksData, null, 2));
     res.redirect('/');
+});
+
+// Route pour afficher les informations d'un livre
+app.get('/book/:id', function(req, res) {
+    const bookId = parseInt(req.params.id);
+    const book = booksData.find(book => book.id === bookId);
+    if (book) {
+        res.render('bookDetails', { book: book, page: 'Livre', request: req });
+    } else {
+        res.status(404).render('error', { page: 'Erreur', request: req });
+    }
 });
 
 // Route pour la page pour suppression de livres dans booksData.json
@@ -47,14 +64,20 @@ app.get('/deleteBook', function(req, res) {
 
 // Route pour supprimer un livre
 app.post('/deleteBook/:id', function(req, res){
+    // Récupérer l'ID du livre à supprimer depuis le fichier booksData.json
     const bookId = parseInt(req.params.id);
-    const bookIndex = booksData.findIndex(book => book.id === bookId);
-    if (bookIndex !== -1) {
-        booksData.splice(bookIndex, 1);
+    const book = booksData.findIndex(book => book.id === bookId);
+    // Vérifier si l'index d'un des livres de booksData.json choisi est valide 
+    if (bookId!== -1) {
+        // Supprimer le livre de booksData.json
+        booksData.splice(book, 1);
+        // Réécrire les données dans le fichier
         fs.writeFileSync(booksDataPath, JSON.stringify(booksData, null, 2));
+        // Redirection vers la page d'accueil
         res.redirect('/');
     } else {
-        res.status(404).render('error', { page: 'Erreur', request: req });
+        // Si le livre n'est pas trouvé, renvoyer une erreur 404
+        res.status(404).send('Livre non trouvé.');
     }
 });
 
@@ -65,42 +88,46 @@ app.get('/borrowBook', function(req, res) {
 
 // Route pour emprunter un livre
 app.post('/borrowBook/:id', function (req, res) {
+    // Récupérer l'ID du livre disponible à l'emprunt depuis le fichier booksData.json
     const bookId = parseInt(req.params.id);
-    const book = booksData.find(book => book.id === bookId);
-    if (book) {
-        if (!book.borrowed) {
-            book.borrowed = true;
-            book.dueDate = req.body.dueDate;
-            fs.writeFileSync(booksDataPath, JSON.stringify(booksData, null, 2));
-            res.redirect('/');
-        } else {
-            res.status(400).send('Le livre est déjà emprunté.');
-        }
+    const book = booksData.findIndex(book => book.id === bookId);
+    // Vérifier si l'index d'un des livres de booksData.json choisi est valide
+    if (bookId !== -1) {
+        // Supprimer un livre disponible pour être emprunté
+        booksData.splice(book, 1);  
+        // Réécrire les données dans le fichier
+        fs.writeFileSync(booksDataPath, JSON.stringify(booksData, null, 2));
+        // Redirection vers la page d'accueil
+        res.redirect('/');
     } else {
+        // Si le livre n'est pas trouvé, renvoyer une erreur 404
         res.status(404).send('Livre non trouvé.');
-    }
+    }   
 });
 
 // Route pour la page de retour de livres dans booksData.json
 app.get('/returnBook', function(req, res) {
     res.render('returnBook', { page: 'Retourner un livre', request: req });
 });
+
 // Route pour retourner un livre
 app.post('/returnBook/:id', function(req, res) {
-    const bookId = parseInt(req.params.id);
-    const book = booksData.find(book => book.id === bookId);
-    if (book) {
-        if (book.borrowed) {
-            book.borrowed = false;
-            book.dueDate = '';
-            fs.writeFileSync(booksDataPath, JSON.stringify(booksData, null, 2));
-            res.redirect('/');
-        } else {
-            res.status(400).send('Le livre n\'a pas été emprunté.');
-        }
-    } else {
-        res.status(404).send('Livre non trouvé.');
-    }
+    const Id = Math.max(...booksData.map(book => book.id));
+    const newBookId = Id + 1;
+    const newBookTitle = req.body.title;
+    const newBookAuthor = req.body.author;
+    const newBookGenre = req.body.genre;
+    const newBookDueDate = req.body.dueDate;
+    booksData.push({
+        id: newBookId,
+        title: newBookTitle,
+        author: newBookAuthor,
+        genre: newBookGenre,
+        borrowed: false,
+        dueDate: newBookDueDate,
+    });
+    fs.writeFileSync(booksDataPath, JSON.stringify(booksData, null, 2));
+    res.redirect('/');
 });
 
 // Lancement du serveur
